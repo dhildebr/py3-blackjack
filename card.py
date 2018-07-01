9e91065e5c2691b3cfbcba6ce9bb8ff1c633d6dd
Daniel Hildebrandt <daniel.s.hildebrandt@gmail.com>
2018-06-30 21:59:46 -0700

    Got decks going, and added docstrings.

--- card.py
+++ card.py
@@ -1,6 +1,17 @@
 class Card():
   """
-  A playing card from the standard 52-card desk.
+  A playing card from the standard 52-card deck. Cards have one of four
+  suits and one of thirteen ranks - an Ace is assumed to have a value of
+  one, though this may vary depending on the game played.
+  
+  Helpful constant attributes:
+  CARD_RANKS: A tuple of all possible card ranks - numbers two through
+  ten, aces, and the three standard face cards.
+  CARD_SUITS: A tuple of the four possible card suits - clubs, diamonds,
+  hearts, and spades.
+  RANK_VALUES: A tuple filled with further 2-tuples mapping each rank
+  name with its numerical value. The first element is the rank as a
+  string, and the second is an integer for the corresponding value.
   """
   
   RANK_VALUES = (
@@ -13,6 +24,14 @@ class Card():
   CARD_SUITS = ("Clubs", "Diamonds", "Hearts", "Spades")
   
   def __init__(self, rank, suit):
+    """
+    Initializes this playing card with the given rank and suit. The
+    arguments provide must be strings. If the rank given is not in
+    CARD_RANKS, it will default to the first element thereof. If the
+    given suit is not in CARD_SUITS, it will similarly default to the
+    first element of that.
+    """
+    
     caps_rank, caps_suit = rank.capitalize(), suit.capitalize()
     self._rank = (caps_rank if (caps_rank in self.CARD_RANKS) else self.CARD_RANKS[0])
     self._suit = (caps_suit if (caps_suit in self.CARD_SUITS) else self.CARD_SUITS[0])
@@ -21,13 +40,21 @@ class Card():
         self._value = val
   
   def get_rank(self):
+    """ Returns this card's rank as a string. """
     return self._rank
   
   def get_suit(self):
+    """ Returns this card's suit as a string. """
     return self._suit
   
   def get_value(self):
+    """ Returns this card's numerical value as an integer. """
     return self._value
   
   def __str__(self):
+    """
+    Returns a string representation of this playing card in the format
+    "{rank} of {suit}". For example, the "Ace of Clubs".
+    """
+    
     return f"{self._rank} of {self._suit}"
---
+++ deck.py
@@ -0,0 +1,43 @@
+import random
+
+from card import Card
+
+class Deck():
+  """
+  A standard deck of 52 cards. Drawing from the desk will return
+  instances of Card until the deck is exhausted, after which doing so
+  will return None.
+  """
+  
+  def __init__(self, init_shuffle = True):
+    """
+    Initializes this deck with the usual 52 cards, formed of the
+    combinations of Card.CARD_RANKS and Card.CARD_SUITS. The deck will
+    be shuffled upon initialization.
+    """
+    
+    self._cards = [Card(rank, suit) for rank in Card.CARD_RANKS for suit in Card.CARD_SUITS]
+    self._num_cards_drawn = 0
+    random.shuffle(self._cards)
+  
+  def is_empty(self):
+    """ Returns whether this deck is exhausted of cards. """
+    return self._num_cards_drawn < len(self._cards)
+  
+  def shuffle(self):
+    """ Shuffles this deck, effectively reconstructing it. """
+    random.shuffle(self._cards)
+    self._num_cards_drawn = 0
+  
+  def draw_card(self):
+    """
+    Returns the next card this deck has to offer, or None if it
+    is empty.
+    """
+    
+    if not self.is_empty():
+      drawn = self._cards[self._num_cards_drawn]
+      self._num_cards_drawn += 1
+      return drawn
+    else:
+      return None
