e6cc91fd0d719557bbabd20f1f2dc97be338443e
Daniel Hildebrandt <daniel.s.hildebrandt@gmail.com>
2018-07-22 13:43:10 -0700

    Added a final bit of information for the player at the gamme's beginning and end.

--- blackjack.py
+++ blackjack.py
@@ -9,7 +9,9 @@ if __name__ == "__main__":
   print("Welcome to Blackjack. Try not to lose your shirt.")
   print("If at any point you need some help, type \"help\" at any input prompt.")
   
-  blackjack = Game()
+  blackjack = Game(100.00)
+  print(f"You'll be starting out with ${blackjack.player_money()} to your name.")
+  
   continue_game = True
   while continue_game:
     bet_amt = parse_reply_bet_amt("How much would you like to bet? ")
@@ -17,3 +19,4 @@ if __name__ == "__main__":
     
     print(f"You have ${blackjack.player_money()} remaining.")
     continue_game = parse_reply_yn("Do you want to play again? ")
+  print("Goodbye.")
